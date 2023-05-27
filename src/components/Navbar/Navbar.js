0a6399a0e2c4dae8799455bad23948fd0b8aec68
KireMitrov <kire_val@yahoo.com>
2023-05-27 21:31:49 +0200

    Inserted burger menu

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,27 @@
-import React from "react";
-import "./navbar.css"
+import React, { useRef } from "react";
+import "./navbar.css";
+
 
 function NavBar() {
-    
-    return(
+    const navRef = useRef();
+
+    const toggleNavbar = () => {
+        navRef.current.classList.toggle("responsive-nav");
+    }
+
+    return (
         <div className="nav-bar-container">
-            <p><a href="#home">Home</a></p>
-            <p><a href="#about">About</a></p>
-            <p><a href="#projects">Projects</a></p>
-            <p><a href="#contact">Contact</a></p>
-            
+            <nav ref={navRef}>
+                <a href="#home" onClick={toggleNavbar}>Home</a>
+                <a href="#about" onClick={toggleNavbar}>About</a>
+                <a href="#projects" onClick={toggleNavbar}>Projects</a>
+                <a href="#contact" onClick={toggleNavbar}>Contact</a>
+                <button onClick={toggleNavbar} className=" nav-btn nav-close-btn"><i className="fa fa-times"></i></button>
+            </nav>
+            <button onClick={toggleNavbar} className="nav-btn"><i className="fa fa-bars"></i></button>
         </div>
     )
 }
 
 export default NavBar
+
