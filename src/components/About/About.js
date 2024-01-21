3185beb581fa2c8c8b72df68b40467c8bb38a062
KireMitrov <kire_val@yahoo.com>
2024-01-21 12:30:42 +0100

    Added some animations

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./about.css"
 import Slider from '../Slider/Slider';
+import { Fade } from "react-awesome-reveal";
 
 
 
@@ -8,13 +9,17 @@ function About() {
 
     return (
         <div className="about-container" id="about">
-            <div className="about-header">About me</div>
-            <div className="about-text">
-                <div>I am Kire, Front-End Developer. I completed an 10-month coding BootCamp and have a deep understanding of the latest development practices and techniques, including proficiency in HTML/CSS, JavaScript, React.js, and Node.js. I have also used the months after the bootcamp to keep sharpening my skills.</div>
-                <div>My college background in Engineering helps me approach Front-End development with a unique problem-solving perspective, allowing me to find innovative solutions to complex problems.</div>
-                <div>With my combination of skills and experience, I am a valuable addition to any team looking for a Front-End Developer who is committed to delivering exceptional results. I am particularly passionate about React.js and enjoy creating dynamic and responsive web applications.</div>
-                <div>If you are interested in learning more about my skills and experience with Front-End development, please feel free to contact me.</div>
-            </div>
+            <Fade direction="up" duration={2000}>
+                <div className="about-header">About me</div>
+            </Fade>
+            <Fade direction="up" duration={2000} delay={500}>
+                <div className="about-text">
+                    <div>I am Kire, Front-End Developer. I completed an 10-month coding BootCamp and have a deep understanding of the latest development practices and techniques, including proficiency in HTML/CSS, JavaScript, React.js, and Node.js. I have also used the months after the bootcamp to keep sharpening my skills.</div>
+                    <div>My college background in Engineering helps me approach Front-End development with a unique problem-solving perspective, allowing me to find innovative solutions to complex problems.</div>
+                    <div>With my combination of skills and experience, I am a valuable addition to any team looking for a Front-End Developer who is committed to delivering exceptional results. I am particularly passionate about React.js and enjoy creating dynamic and responsive web applications.</div>
+                    <div>If you are interested in learning more about my skills and experience with Front-End development, please feel free to contact me.</div>
+                </div>
+            </Fade>
             <div className="skills">Skills</div>
             <Slider></Slider>
         </div>
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,7 +1,8 @@
 import React from "react";
 import "./projects.css"
-import { Fade, Slide, Zoom } from 'react-slideshow-image';
+import { Slide, Zoom } from 'react-slideshow-image';
 import "react-slideshow-image/dist/styles.css"
+import { Fade } from 'react-awesome-reveal'
 
 function Projects() {
 
@@ -11,7 +12,7 @@ function Projects() {
             projectImages: ["cart1", "cart2", "cart3"],
             description: `E-commerce shoping cart made with ReactJS class components. The shopping cart have multiple categories, cart overlay view and cart view. All the poducts have multiple attributes which you can change either in cart page either in cart-overlay.`,
             stack: ["JavaScript", "ReactJS", "GraphQL"],
-            linkGithub:"https://github.com/KireMitrov/Shopping-cart-classes",
+            linkGithub: "https://github.com/KireMitrov/Shopping-cart-classes",
             linkLive: "https://cart-kire.netlify.app/"
         },
         {
@@ -19,7 +20,7 @@ function Projects() {
             projectImages: ["todolist"],
             description: `Todo App made with ReactJS. You can add, edit and delete tasks. The tasks are saved in local storage and available after closing the window.`,
             stack: ["Html", "CSS", "JavaScript", "ReactJS"],
-            linkGithub:"https://github.com/KireMitrov/ReactJs-To-Do-App",
+            linkGithub: "https://github.com/KireMitrov/ReactJs-To-Do-App",
             linkLive: "https://todoapp-kire.netlify.app/"
         },
         {
@@ -27,7 +28,7 @@ function Projects() {
             projectImages: ["cryptocoins1", "cryptocoins2", "cryptocoins3"],
             description: `crypto price tracking app using the React and the Coingecko Api.`,
             stack: ["ReactJs", "Coingecko Api", "Axios", "Recharts"],
-            linkGithub:"https://github.com/KireMitrov/crypto-coins",
+            linkGithub: "https://github.com/KireMitrov/crypto-coins",
             linkLive: "https://crypto-info-kire.netlify.app/"
         },
         {
@@ -35,7 +36,7 @@ function Projects() {
             projectImages: ['weatherapp1'],
             description: `Weather app which initialy is showing you the weather of the current positio. There is search bar where you can find and display the weather up to your wish`,
             stack: ["Html", "CSS", "JavaScript", "ReactJS"],
-            linkGithub:"https://github.com/KireMitrov/React-Weather-App",
+            linkGithub: "https://github.com/KireMitrov/React-Weather-App",
             linkLive: "https://kire-weatherapp.netlify.app/"
         },
         {
@@ -43,7 +44,7 @@ function Projects() {
             projectImages: ["progressbar1", "progressbar2", "progressbar3"],
             description: `Step progress bar which is showing you on where exactly is your shopping experince, how many steps you have completed and how many left till you complete your order. `,
             stack: ["Html", "CSS", "JavaScript", "ReactJS"],
-            linkGithub:"https://github.com/KireMitrov/React-ProgressBar-component",
+            linkGithub: "https://github.com/KireMitrov/React-ProgressBar-component",
             linkLive: "https://progressbar-kire.netlify.app/"
         },
         {
@@ -51,15 +52,15 @@ function Projects() {
             projectImages: ["landingpage1", "landingpage2", "landingpage3"],
             description: `This is a landing page made for travel agancies where you can put your credentials in a form and win a trip with a lottery, additonal there is comercial trips which the agency is selling`,
             stack: ["Html", "CSS", "JavaScript", "ReactJS"],
-            linkGithub:"https://github.com/KireMitrov/React-Landing-Page",
+            linkGithub: "https://github.com/KireMitrov/React-Landing-Page",
             linkLive: "https://landingpage-kire.netlify.app/"
         },
         {
             projectName: "Quiz App",
-            projectImages: ["quiz1","quiz2","quiz3","quiz4",],
+            projectImages: ["quiz1", "quiz2", "quiz3", "quiz4",],
             description: `Quiz App made with ReactJS and NodeJs. This app has server done with NodeJs where the data is stored on MongoDb database. It have a Admin page too where you can easily add new questions`,
             stack: ["ReactJS", "NodeJS", "MongoDb"],
-            linkGithub:"https://github.com/KireMitrov",
+            linkGithub: "https://github.com/KireMitrov",
             linkLive: "https://kire-mitrov.netlify.app/"
         },
     ]
@@ -69,26 +70,28 @@ function Projects() {
             <div className="projects-title"> Some of the things I`ve build</div>
             <div className="projects-container">
                 {projects.map((project) => (
-                    <div className="project-card-container">
-                        <div className="project-name">{project.projectName}</div>
-                        <div className="slide-container">
-                            <Slide>
-                                {project.projectImages.map((fadeImage, index) => (
-                                    <div key={index}>
-                                        <img style={{ width: '80%', maxHeight: '50%' }} src={require(`../../assets/images/projects/${fadeImage}.png`)} />
-                                    </div>
-                                ))}
-                            </Slide>
+                    <Fade direction="left" duration={1500}>
+                        <div className="project-card-container">
+                            <div className="project-name">{project.projectName}</div>
+                            <div className="slide-container">
+                                <Slide>
+                                    {project.projectImages.map((fadeImage, index) => (
+                                        <div key={index}>
+                                            <img style={{ width: '80%', maxHeight: '50%' }} src={require(`../../assets/images/projects/${fadeImage}.png`)} />
+                                        </div>
+                                    ))}
+                                </Slide>
+                            </div>
+                            <div className="description">{project.description}</div>
+                            <div className="technologies-container">{project.stack.map((technology) => (
+                                <div className="technology">{technology}</div>
+                            ))}</div>
+                            <div className="project-links">
+                                <a href={project.linkGithub} title="Source code" target="_blank"><i class="fab fa-github"></i></a>
+                                <a href={project.linkLive} title="Click to visit the app" target="_blank"><i class="fas fa-globe"></i></a>
+                            </div>
                         </div>
-                        <div className="description">{project.description}</div>
-                        <div className="technologies-container">{project.stack.map((technology) => (
-                            <div className="technology">{technology}</div>
-                        ))}</div>
-                        <div className="project-links">
-                        <a href={project.linkGithub} title="Source code" target="_blank"><i class="fab fa-github"></i></a>
-                        <a href={project.linkLive} title="Click to visit the app" target="_blank"><i class="fas fa-globe"></i></a>
-                        </div>
-                    </div>
+                    </Fade>
                 ))}
             </div>
         </div>
